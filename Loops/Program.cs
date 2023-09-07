010902055f02b1fb257de0afa0123c1225c99174
ninjafiveo <mjsekol@live.com>
2023-09-07 13:02:53 -0400

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ForLoop();
+            Console.ReadKey();
+        }
+
+        static void ForLoop()
+        {
+            int numberOfTacos = 10;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < numberOfTacos; numberOfTacos--)
+            {
+                Console.WriteLine($"I eat a taco and now I have {numberOfTacos} Tacos!");
+                System.Threading.Thread.Sleep(100);
+                Console.WriteLine("nom nom nom nom");
+            }
+
+            Console.WriteLine("Now I am sad because I have no more tacos.");
+
+        }
+    }
+}
